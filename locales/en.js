408b9bf96bc2a82313c1e95e99df57eb6bd54c64
azzmi-my <76639636+azzmi-my@users.noreply.github.com>
2021-01-18 00:56:28 +0800

    added langs scripts and modded the index
    the language files inside a "locale" in the root
    modded the index to simply test the multilanguigal feature

---
+++ locales/en.js
@@ -0,0 +1,105 @@
+export default {
+    Home: 'Home',
+    ContactUs: 'Contact us',
+    AboutUs: 'About us',
+
+    HomePageTitle: 'Main Page',
+    ContactUsPageTitle: 'Where to find us?',
+    AboutUsPageTitle: 'Who are we?',
+
+
+    companyName: 'Dubai Resources Sdn. Bhd.',
+    homeMainTitle: 'A comprehensive range of real estate services',
+    homeMainDescription: 'We always seek to build business partnerships with our customers through the best value offers for such kind of business.',
+
+
+    homeBodyHeadTxt: 'We provide appropriate guidance to our clients with acquiring real estate assets through a combination of in-depth research, analysis, strategies and advice.',
+    homeBodyHeadSubTxt: 'Property and Real Estate Investment within Malaysia is one of our core businesses that we are here for.',
+
+    CompanyRegistrationTitle: 'Company Registration',
+    CompanyRegistrationSubTitle: 'Dubai Resources Sdn Bhd is a reputable and authorized agency incorporation services entity set up to help business owners (Malaysians and non-Malaysians) to register a company and conduct business legally in Malaysia.',
+    CompanyRegistrationDescription: 'Our vision is to establish unique and effective solutions to businesses. \
+    Dubai Resources Sdn Bhd offers a full variety of guidance and advisory services to make certain that clients comply with the applicable legislative necessities at all times. \
+    With a crew of professionally certified Company Secretaries and skilled Staffs, we believe that the selection of services we offer along with our expertise make us a complete one-stop center for Corporate Secretarial needs. \
+    Our Secretarial services are as follows: \
+    • General consultation of compliance with the legislations. \
+    • Formation of Company, i.e. Sdn. Bhd., Bhd. \
+    • Named / Registered secretary for your company. \
+    • Registered address for your company. \
+    • Official corporate mail correspondence with relevant Malaysia government divisions. \
+    • Arrangement of board & general meeting \
+    • Making sure all the documentations comply with relevant regulations and CA, lodgment of annual return and audited account to CCM. \
+    Company registration, accounting, income tax, auditing and business consulting. \
+    We analyze investment rules, legal issue, free zone facilities, create major industries, export and import rules, banking policy, land rental or lease, low taxes, transfer of a portion of profits, etc., to each project to help investors take appropriate measures for a trading decision such as promoters Abroad. \
+    We help our clients to make a significant and lasting improvement to their business, through our service. \
+    We also maintain the confidentiality of our clients and do not share any information with others.',
+    
+    FranchisingTitle: 'Franchising',
+    FranchisingSubTitle: 'Dubai Resources Sdn Bhd provides franchise consultancy services to foreign investors within Malaysia. Purchasing a franchise cab be a suitable alternative so that the client can start his business.',
+    FranchisingDescription: 'The investors can have a trade-mark, products, a price and marketing materials ready for use. Our innovative franchise packages provide assistance to foreigners starting a franchise enterprise in Malaysia. The services integrate personalized sessions, strategic planning and operational information to offer clients a turnkey business acquisition solution to begin a franchise commercial enterprise in Malaysia. \
+    In making use of our sources and experience, we make certain that prospective franchise buyers make intelligent investment choices in commercial enterprise and the future. There is a total of 586 franchise corporations in the Malaysia Franchise Industry and it has been up-trending with a steady boom of 15% year-on-year since 2008. \
+    To sign in a franchise with the Franchise Registry, the following documents ought to be lodged on the Registry by the franchisor: \
+    • A Franchise Agreement \
+    • A Disclosure Document (Form BAF1) \
+    • Registration Application Form (Form BAF2) \
+    • Operation Manual \
+    • Training Manual \
+    • Certificate of Incorporation \
+    • Forms 24 and forty nine (Company’s return as to the Board of Directors and the Shareholders) \
+    • Form of Annual Return of an enterprise having a share capital \
+    • Latest audited stability sheet and earnings and loss account \
+    • Company’s organizational chart, together with the names and function of key personnel \
+    • Company’s business brochures \
+    All of the above documents need to be certified actual copies of the report.',
+
+    LegalServicesTitle: 'Legal Services',
+    LegalServicesSubTitle: 'Dubai Resources Sdn Bhd offers an extensive variety of legal services for those who are interested in investing in Malaysia; from business enterprise incorporation to representation in front of the local courts for all ranges of legal matters. Our lawyers in Malaysia are specialized in more than one area of regulation, such as company regulation, public procurement regulation, criminal business law or industrial and hard labour relations. It allows us to provide solutions to the companies and enables us to finalize the processes and close them efficiently.',
+    LegalServicesDescription: 'Dubai Resources Sdn Bhd offers an extensive variety of legal services for those who are interested in investing in Malaysia; from business enterprise incorporation to representation in front of the local courts for all ranges of legal matters. Our lawyers in Malaysia are specialized in more than one area of regulation, such as company regulation, public procurement regulation, criminal business law or industrial and hard labour relations. It allows us to provide solutions to the companies and enables us to finalize the processes and close them efficiently. \
+    Our legal team in Malaysia provides professional consultancy services in addition to issues related to local taxation and other financial matters clients may come across whilst setting up a business in Malaysia. Our legal team is well-trained and educated in the local laws and regulations, and are therefore qualified to offer the best counsel for our client in any legal matters. \
+    In addition, we believe that the access to the legal services should not constitute an obstacle to the growth and expansion of our clients’ businesses to their highest levels. That is why we stand by you through our advice and concern. We have gained our experience and knowledge  by offering consultation to the clients regarding the commercial processes and offering solutions to the conflicts occurring in the industrial relations and the civil law to curb the complicated issues faced by the technology companies, the emerging companies, social institutions, and the small and medium enterprises (SMEs). Moreover, we offer other services such as writing and reviewing contracts and documents of contracts, offering legal advice, representing clients in cases of fraudulence and court cases, and the legal representation when our clients are arrested by the police or by the claims of insurance and accidents. \
+    Our legal team has been established since 2012 with good knowledge of the laws of Malaysia. We appreciate our clients’ needs and try to communicate with them so that we can understand and respond to them effectively through our lawyers who enjoy hogh proficiency and our supporting staff. Our main aim is adding a value to our works through providing our clients with fast and efficient legal solutions that help them to improve their success. Concentrating on the practical implementation and the creative legal solutions, our excellence is related to our personal customer service and a large group of practicing areas. We are proud with our legal services we offer to an increasing number of financial institutions, real estate developers, multi-national companies and the companies of the country.',
+
+    RealEstateTitle: 'Property and Real Estate Investment',
+    RealEstateSubTitle: 'Property and Real Estate Investment within Malaysia is one of our core businesses that we provide to local and international investors.',
+    RealEstateDescription: 'The Malaysian real estate has been growing rapidly, creating lucrative investment opportunities, which has attracted a huge number of investors. \
+    We provide appropriate guidance to our clients with acquiring real estate assets through a combination of in-depth research, analysis, strategies and advice. \
+    We offer investors our professional services which are sourcing for and assisting in purchasing properties (residential, commercial, industrial, and leisure) and land banking based on the investors’ specific requirements and budget. \
+    Hence, we advise on status updates on the economic and investment climate, real estate investment guidelines and investment opportunities available to foreign companies and investors. \
+    Our team also assists with the application and procedures of the property purchase to ensure a complete and successful transfer. \
+    So far, we have completed many projects of our customers such as agricultural lands and palm trees plantation, carried out feasibility studies to build petrol pumping station and many other projects which are proposals for residential projects around Klang Valley. \
+    We know the local market largely and are able to find a real estate that meet the investors’ standards so that they can decrease the risks to their minimum level and simultaneously increase the investment revenue to its maximum. \
+    We can assist in the legal issues including address, investigation studies and feasibility before proceeding to the next steps. With the standard operating procedures we adopt, we insure that all negotiations, transactions and operations are transparently presented to all investors to protect their interests. \
+    We took part in a big number of commercial real estates including office buildings, restaurants, groceries, and many similar activities.',
+
+    ImportExportTitle: 'Import and Export',
+    ImportExportSubTitle: 'Dubai Resources Sdn. Bhd. is one of the trading companies in the market that is specialized in import, export, distribution, packaging and marketing of food and consumer products to other parties under our own brand or customers’ chosen brand.',
+    ImportExportDescription: 'As an exporter, we offer quality and premium items at reasonable rates, thus consequently instilling our clients’ confidence in us. As like any other business, our goal is to maximize profits, however, we also strongly believe in customer satisfaction and delight. \
+    We have committed ourselves to offer the best products with the best quality and prices. We always seek to build business partnerships with our customers through the best value offers for such kind of business. \
+    Dubai Resources Sdn Bhd is a well-known distributor of foodstuff, electrical and electronics, home and personal care products, and other FMCG within Malaysia as well as in the Middle East. \
+    Dubai Resources Sdn Bhd has gained noticeable growth in the Malaysian market taking in account that Malaysian market is one of the most competitive markets in the South East Asia, and we continue to strive to gain more of the market share across the region. Representing other firms’ products has always enabled us to gain consumers’ trust and have a competitive edge across the region to expand to other emerging markets. \
+    Dubai Resources Sdn Bhd has set strategies and goals based on the precept of leading consumer satisfaction through product quality, reliability and efficient service by supplying an extensive range of merchandise with competitive prices. We diligently seek to particularly cater for our customers’ needs and hold strong belief in keeping strong ties with them.',
+
+
+    WhoAreWe: 'Dubai Resources Sdn. Bhd. is one of the trading companies in the market that is specialized in import, export, distribution, packaging and marketing of food and consumer products to other parties under our own brand or customers’ chosen brand. \
+    As an exporter, we offer quality and premium items at reasonable rates, thus consequently instilling our clients’ confidence in us. \
+    As like any other business, our goal is to maximize profits, however, we also strongly believe in customer satisfaction and delight. \
+    We have committed ourselves to offer the best products with the best quality and prices. \
+    We always seek to build business partnerships with our customers through the best value offers for such kind of business.',
+
+
+    ContactUsInfo: {
+        phoneTxt: 'phone number',
+        phone: '+60 351034560',
+        emailTxt: '',
+        email: '',
+        locationTxt: 'Location',
+        location: 'NO 27A, JALAN TS 6/10A, \
+        TAMAN INDUSTRI SUBANG, \
+        47500 SUBANG JAYA \
+        SELANGOR',
+    },
+
+
+    copyrights: '2020 Creative Wonders Digital, All rights reserved',
+
+}
---
+++ locales/ms.js
@@ -0,0 +1,50 @@
+export default {
+    Home: ,
+    ContactUs: ,
+    AboutUs: ,
+
+    HomePageTitle: .
+    ContactUsPageTitle: .
+    AboutUsPageTitle: .
+
+
+    companyName: ,
+    homeMainTitle: ,
+    homeMainDescription: ,
+
+
+    homeBodyHeadTxt: ,
+
+    CompanyRegistrationTitle: ,
+    CompanyRegistrationSubTitle: ,
+    CompanyRegistrationDescription: ,
+    FranchisingSubTitle: ,
+    FranchisingDescription: ,
+    FranchisingTitle: ,
+    RealEstateTitle: ,
+    RealEstateSubTitle: ,
+    RealEstateDescription: ,
+    LegalServicesTitle: ,
+    LegalServicesSubTitle: ,
+    LegalServicesDescription: ,
+    ImportExportTitle: ,
+    ImportExportSubTitle: ,
+    ImportExportDescription: ,
+
+
+    WhoAreWe: ,
+
+
+    ContactUsInfo: {
+        phoneTxt: ,
+        phone: ,
+        emailTxt: ,
+        email: ,
+        locationTxt: ,
+        location: ,
+    },
+
+
+    copyrights: ,
+
+}
